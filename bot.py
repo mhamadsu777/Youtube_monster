4aa8d08be9ac129e7ac09d76a08ce4cfccc15877
mhamadsu777 <66016885+mhamadsu777@users.noreply.github.com>
2021-01-24 09:09:26 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ from multiprocessing import Process
 from youtubesearchpython import SearchVideos
 
 
-bot = telepotpro.Bot("1150098316:AAGLROio2c3DEo2si1dzOfTWIRz87Xp0pOE")
+bot = telepotpro.Bot("Token")
 
 def startMsg(chat_id, first_name):
 	bot.sendMessage(chat_id, '*ميزة اليوتيوب تعمل بشكل جيد*', parse_mode= 'Markdown')
